bff8c73294f0748244a83ede5b137acc4d82fca0
forevertux <tux@forevertux.tux>
2025-03-08 14:20:00 +0200

    Add entity classes

---
+++ src/Entities/ProductEntity.pas
@@ -0,0 +1,139 @@
+unit ProductEntity;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, System.Generics.Collections;
+
+type
+  TProduct = class
+  private
+    FProductID: Integer;
+    FProductCode: string;
+    FProductName: string;
+    FDescription: string;
+    FCategoryID: Integer;
+    FCategoryName: string;
+    FUnitPrice: Double;
+    FCostPrice: Double;
+    FQuantity: Integer;
+    FMinStockLevel: Integer;
+    FMaxStockLevel: Integer;
+    FBranchID: Integer;
+    FBranchName: string;
+    FBarcode: string;
+    FSKU: string;
+    FIsActive: Boolean;
+    FCreatedBy: Integer;
+    FCreatedAt: TDateTime;
+    FUpdatedAt: TDateTime;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    procedure Clear;
+    function GetStockStatus: string;
+    function IsLowStock: Boolean;
+    function IsOutOfStock: Boolean;
+    function IsOverStock: Boolean;
+    function GetProfitMargin: Double;
+    function CanSell(RequestedQuantity: Integer): Boolean;
+
+    property ProductID: Integer read FProductID write FProductID;
+    property ProductCode: string read FProductCode write FProductCode;
+    property ProductName: string read FProductName write FProductName;
+    property Description: string read FDescription write FDescription;
+    property CategoryID: Integer read FCategoryID write FCategoryID;
+    property CategoryName: string read FCategoryName write FCategoryName;
+    property UnitPrice: Double read FUnitPrice write FUnitPrice;
+    property CostPrice: Double read FCostPrice write FCostPrice;
+    property Quantity: Integer read FQuantity write FQuantity;
+    property MinStockLevel: Integer read FMinStockLevel write FMinStockLevel;
+    property MaxStockLevel: Integer read FMaxStockLevel write FMaxStockLevel;
+    property BranchID: Integer read FBranchID write FBranchID;
+    property BranchName: string read FBranchName write FBranchName;
+    property Barcode: string read FBarcode write FBarcode;
+    property SKU: string read FSKU write FSKU;
+    property IsActive: Boolean read FIsActive write FIsActive;
+    property CreatedBy: Integer read FCreatedBy write FCreatedBy;
+    property CreatedAt: TDateTime read FCreatedAt write FCreatedAt;
+    property UpdatedAt: TDateTime read FUpdatedAt write FUpdatedAt;
+  end;
+
+  TProductList = TObjectList<TProduct>;
+
+implementation
+
+uses
+  Constants;
+
+{ TProduct }
+
+constructor TProduct.Create;
+begin
+  inherited Create;
+  Clear;
+end;
+
+destructor TProduct.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TProduct.Clear;
+begin
+  FProductID := 0;
+  FProductCode := '';
+  FProductName := '';
+  FDescription := '';
+  FCategoryID := 0;
+  FCategoryName := '';
+  FUnitPrice := 0.0;
+  FCostPrice := 0.0;
+  FQuantity := 0;
+  FMinStockLevel := 0;
+  FMaxStockLevel := 0;
+  FBranchID := 0;
+  FBranchName := '';
+  FBarcode := '';
+  FSKU := '';
+  FIsActive := True;
+  FCreatedBy := 0;
+  FCreatedAt := Now;
+  FUpdatedAt := Now;
+end;
+
+function TProduct.GetStockStatus: string;
+begin
+  Result := Constants.GetStockStatus(FQuantity, FMinStockLevel, FMaxStockLevel);
+end;
+
+function TProduct.IsLowStock: Boolean;
+begin
+  Result := (FQuantity > 0) and (FQuantity <= FMinStockLevel);
+end;
+
+function TProduct.IsOutOfStock: Boolean;
+begin
+  Result := FQuantity <= 0;
+end;
+
+function TProduct.IsOverStock: Boolean;
+begin
+  Result := (FMaxStockLevel > 0) and (FQuantity >= FMaxStockLevel);
+end;
+
+function TProduct.GetProfitMargin: Double;
+begin
+  if FCostPrice > 0 then
+    Result := ((FUnitPrice - FCostPrice) / FCostPrice) * 100
+  else
+    Result := 0;
+end;
+
+function TProduct.CanSell(RequestedQuantity: Integer): Boolean;
+begin
+  Result := (RequestedQuantity > 0) and (FQuantity >= RequestedQuantity) and FIsActive;
+end;
+
+end.
---
+++ src/Entities/UserEntity.pas
@@ -0,0 +1,137 @@
+unit UserEntity;
+
+interface
+
+uses
+  System.SysUtils, System.Classes;
+
+type
+  TUser = class
+  private
+    FUserID: Integer;
+    FUsername: string;
+    FPasswordHash: string;
+    FFullName: string;
+    FEmail: string;
+    FPhone: string;
+    FRoleID: Integer;
+    FBranchID: Integer;
+    FIsActive: Boolean;
+    FLastLogin: TDateTime;
+    FCreatedAt: TDateTime;
+    FUpdatedAt: TDateTime;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    procedure Clear;
+    function IsAdmin: Boolean;
+    function IsManager: Boolean;
+    function IsEmployee: Boolean;
+    function CanAccessInventory: Boolean;
+    function CanAccessSales: Boolean;
+    function CanAccessReports: Boolean;
+    function CanAccessUserManagement: Boolean;
+    function CanEditProduct: Boolean;
+    function CanDeleteProduct: Boolean;
+    function CanProcessSale: Boolean;
+
+    property UserID: Integer read FUserID write FUserID;
+    property Username: string read FUsername write FUsername;
+    property PasswordHash: string read FPasswordHash write FPasswordHash;
+    property FullName: string read FFullName write FFullName;
+    property Email: string read FEmail write FEmail;
+    property Phone: string read FPhone write FPhone;
+    property RoleID: Integer read FRoleID write FRoleID;
+    property BranchID: Integer read FBranchID write FBranchID;
+    property IsActive: Boolean read FIsActive write FIsActive;
+    property LastLogin: TDateTime read FLastLogin write FLastLogin;
+    property CreatedAt: TDateTime read FCreatedAt write FCreatedAt;
+    property UpdatedAt: TDateTime read FUpdatedAt write FUpdatedAt;
+  end;
+
+implementation
+
+uses
+  Constants;
+
+{ TUser }
+
+constructor TUser.Create;
+begin
+  inherited Create;
+  Clear;
+end;
+
+destructor TUser.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TUser.Clear;
+begin
+  FUserID := 0;
+  FUsername := '';
+  FPasswordHash := '';
+  FFullName := '';
+  FEmail := '';
+  FPhone := '';
+  FRoleID := 0;
+  FBranchID := 0;
+  FIsActive := True;
+  FLastLogin := 0;
+  FCreatedAt := Now;
+  FUpdatedAt := Now;
+end;
+
+function TUser.IsAdmin: Boolean;
+begin
+  Result := FRoleID = ROLE_ADMIN;
+end;
+
+function TUser.IsManager: Boolean;
+begin
+  Result := FRoleID = ROLE_MANAGER;
+end;
+
+function TUser.IsEmployee: Boolean;
+begin
+  Result := FRoleID = ROLE_EMPLOYEE;
+end;
+
+function TUser.CanAccessInventory: Boolean;
+begin
+  Result := IsAdmin or IsManager or IsEmployee;
+end;
+
+function TUser.CanAccessSales: Boolean;
+begin
+  Result := IsAdmin or IsManager or IsEmployee;
+end;
+
+function TUser.CanAccessReports: Boolean;
+begin
+  Result := IsAdmin or IsManager;
+end;
+
+function TUser.CanAccessUserManagement: Boolean;
+begin
+  Result := IsAdmin;
+end;
+
+function TUser.CanEditProduct: Boolean;
+begin
+  Result := IsAdmin or IsManager;
+end;
+
+function TUser.CanDeleteProduct: Boolean;
+begin
+  Result := IsAdmin;
+end;
+
+function TUser.CanProcessSale: Boolean;
+begin
+  Result := IsAdmin or IsManager or IsEmployee;
+end;
+
+end.
