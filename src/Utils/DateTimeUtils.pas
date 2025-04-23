21aca1455e4216ca49f79d907d23655e9a36382c
forevertux <tux@forevertux.tux>
2025-04-23 15:20:00 +0300

    Final refinements

---
+++ src/Utils/DateTimeUtils.pas
@@ -0,0 +1,102 @@
+unit DateTimeUtils;
+
+interface
+
+uses
+  System.SysUtils, System.DateUtils;
+
+function FormatDateTimeForDB(const DateTime: TDateTime): string;
+function FormatDateForDB(const Date: TDateTime): string;
+function FormatDateTimeForDisplay(const DateTime: TDateTime): string;
+function FormatDateForDisplay(const Date: TDateTime): string;
+function ParseDBDateTime(const DateTimeStr: string): TDateTime;
+function GetCurrentDateTime: TDateTime;
+function GetCurrentDate: TDateTime;
+function GetStartOfDay(const DateTime: TDateTime): TDateTime;
+function GetEndOfDay(const DateTime: TDateTime): TDateTime;
+function GetStartOfMonth(const DateTime: TDateTime): TDateTime;
+function GetEndOfMonth(const DateTime: TDateTime): TDateTime;
+function DaysBetween(const StartDate, EndDate: TDateTime): Integer;
+function IsToday(const DateTime: TDateTime): Boolean;
+function IsThisMonth(const DateTime: TDateTime): Boolean;
+
+implementation
+
+uses
+  Constants;
+
+function FormatDateTimeForDB(const DateTime: TDateTime): string;
+begin
+  Result := FormatDateTime(DATETIME_FORMAT, DateTime);
+end;
+
+function FormatDateForDB(const Date: TDateTime): string;
+begin
+  Result := FormatDateTime(DATE_FORMAT, Date);
+end;
+
+function FormatDateTimeForDisplay(const DateTime: TDateTime): string;
+begin
+  Result := FormatDateTime(DISPLAY_DATETIME_FORMAT, DateTime);
+end;
+
+function FormatDateForDisplay(const Date: TDateTime): string;
+begin
+  Result := FormatDateTime(DISPLAY_DATE_FORMAT, Date);
+end;
+
+function ParseDBDateTime(const DateTimeStr: string): TDateTime;
+begin
+  try
+    Result := StrToDateTime(DateTimeStr);
+  except
+    Result := 0;
+  end;
+end;
+
+function GetCurrentDateTime: TDateTime;
+begin
+  Result := Now;
+end;
+
+function GetCurrentDate: TDateTime;
+begin
+  Result := Date;
+end;
+
+function GetStartOfDay(const DateTime: TDateTime): TDateTime;
+begin
+  Result := StartOfTheDay(DateTime);
+end;
+
+function GetEndOfDay(const DateTime: TDateTime): TDateTime;
+begin
+  Result := EndOfTheDay(DateTime);
+end;
+
+function GetStartOfMonth(const DateTime: TDateTime): TDateTime;
+begin
+  Result := StartOfTheMonth(DateTime);
+end;
+
+function GetEndOfMonth(const DateTime: TDateTime): TDateTime;
+begin
+  Result := EndOfTheMonth(DateTime);
+end;
+
+function DaysBetween(const StartDate, EndDate: TDateTime): Integer;
+begin
+  Result := System.DateUtils.DaysBetween(StartDate, EndDate);
+end;
+
+function IsToday(const DateTime: TDateTime): Boolean;
+begin
+  Result := System.DateUtils.IsToday(DateTime);
+end;
+
+function IsThisMonth(const DateTime: TDateTime): Boolean;
+begin
+  Result := (YearOf(DateTime) = YearOf(Now)) and (MonthOf(DateTime) = MonthOf(Now));
+end;
+
+end.
---
+++ src/Utils/ValidationUtils.pas
@@ -0,0 +1,88 @@
+unit ValidationUtils;
+
+interface
+
+uses
+  System.SysUtils, System.RegularExpressions;
+
+function IsValidEmail(const Email: string): Boolean;
+function IsValidPhone(const Phone: string): Boolean;
+function IsValidPassword(const Password: string): Boolean;
+function IsPositiveNumber(const Value: Double): Boolean;
+function IsValidQuantity(const Quantity: Integer): Boolean;
+function IsValidPrice(const Price: Double): Boolean;
+function IsEmptyOrWhiteSpace(const Value: string): Boolean;
+function ValidateRequired(const Value: string; const FieldName: string; out ErrorMsg: string): Boolean;
+function ValidateNumericRange(const Value: Double; const Min, Max: Double; const FieldName: string; out ErrorMsg: string): Boolean;
+function ValidateLength(const Value: string; const MinLen, MaxLen: Integer; const FieldName: string; out ErrorMsg: string): Boolean;
+
+implementation
+
+uses
+  Constants;
+
+function IsValidEmail(const Email: string): Boolean;
+const
+  EmailPattern = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+begin
+  Result := TRegEx.IsMatch(Email, EmailPattern);
+end;
+
+function IsValidPhone(const Phone: string): Boolean;
+const
+  PhonePattern = '^\+?[0-9\s\-\(\)]{7,20}$';
+begin
+  Result := TRegEx.IsMatch(Phone, PhonePattern);
+end;
+
+function IsValidPassword(const Password: string): Boolean;
+begin
+  // Password must be at least MIN_PASSWORD_LENGTH characters
+  Result := Length(Password) >= MIN_PASSWORD_LENGTH;
+end;
+
+function IsPositiveNumber(const Value: Double): Boolean;
+begin
+  Result := Value >= 0;
+end;
+
+function IsValidQuantity(const Quantity: Integer): Boolean;
+begin
+  Result := Quantity >= 0;
+end;
+
+function IsValidPrice(const Price: Double): Boolean;
+begin
+  Result := Price >= 0;
+end;
+
+function IsEmptyOrWhiteSpace(const Value: string): Boolean;
+begin
+  Result := Trim(Value) = '';
+end;
+
+function ValidateRequired(const Value: string; const FieldName: string; out ErrorMsg: string): Boolean;
+begin
+  Result := not IsEmptyOrWhiteSpace(Value);
+  if not Result then
+    ErrorMsg := FieldName + ' is required';
+end;
+
+function ValidateNumericRange(const Value: Double; const Min, Max: Double; const FieldName: string; out ErrorMsg: string): Boolean;
+begin
+  Result := (Value >= Min) and (Value <= Max);
+  if not Result then
+    ErrorMsg := Format('%s must be between %.2f and %.2f', [FieldName, Min, Max]);
+end;
+
+function ValidateLength(const Value: string; const MinLen, MaxLen: Integer; const FieldName: string; out ErrorMsg: string): Boolean;
+var
+  Len: Integer;
+begin
+  Len := Length(Value);
+  Result := (Len >= MinLen) and (Len <= MaxLen);
+  if not Result then
+    ErrorMsg := Format('%s must be between %d and %d characters', [FieldName, MinLen, MaxLen]);
+end;
+
+end.
